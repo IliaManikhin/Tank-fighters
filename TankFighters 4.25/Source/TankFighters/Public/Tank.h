5ccd018750f5b369c73cb4c41a3645acbbf306a2
Ilia Manikhin <begbeder2012@yandex.ru>
2021-10-21 12:53:20 +0800

    Use EditDefaultsOnly  for all uproperties

--- TankFighters 4.25/Source/TankFighters/Public/Tank.h
+++ TankFighters 4.25/Source/TankFighters/Public/Tank.h
@@ -44,17 +44,17 @@ public:
 	void Fire();
 
 private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
 
-	UPROPERTY(EditAnywhere, Category = Firing)
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float LaunchSpeed = 4000; 
 
-	UPROPERTY(EditAnywhere, Category = Setup)
-	TSubclassOf<AProjectile> ProjectileBlueprint;  
+	UPROPERTY(EditAnywhere, Category = Firing)
+		float ReloadTimeInSeconds = 3;
 
 	// Local barrel reference for spawn projectiles
 	UTankBarrel* Barrel = nullptr;
-
-	float ReloadTimeInSeconds = 3;
 
 	double LastFireTime = 0;
 	
